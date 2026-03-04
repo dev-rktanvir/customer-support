4cb6b4cac821b7eac4f59354cd30892e9f7aa8b3
rk-tanvir790 <ktanvir790@gmail.com>
2026-03-04 18:57:36 +0600

    complete navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
 
   return (
     <>
+      {/* Header */}
+      <header>
+        <Navbar></Navbar>
+      </header>
 
+      {/* Main */}
+      <main>
+
+      </main>
+
+      {/* Footer */}
+      <footer>
+
+      </footer>
     </>
   )
 }
